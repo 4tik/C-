447636148d6e5ad2098774aaa5dee3f08498aea0
Syntax.Error <atikrah2010@gmail.com>
2023-03-06 17:03:18 +0600

    no message

---
+++ Udemy(C# Complete Master Course (Updated to C# 10))/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+            int integer = 10;
+            Console.WriteLine($"{str}");
+            Console.WriteLine($"{integer}");
+            Console.SetWindowSize(50,50);
+        }
+    }
+}
---
+++ Udemy(C# Complete Master Course (Updated to C# 10))/Statement/Statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "Some String";
+            Console.WriteLine($"{nameof(firstString)} : {firstString}");
+            PrintSomething();
+            Console.WriteLine($"The value of Square : {Square(12)}");
+        }
+
+        static void PrintSomething()
+        {
+            Console.WriteLine("Hello from Print Something method");
+        }
+
+        static int Square(int number)
+        {
+            return (number * number);
+        }
+    }
+}
